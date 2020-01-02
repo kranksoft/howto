770df40784931053d412922338cf2e47e5008033
Terje Innerdal <kranksoft@protonmail.com>
2020-01-02 10:57:49 +0100

    Added network and default values to start_postgres.sh

--- docker/postgres/start_postgres.sh
+++ docker/postgres/start_postgres.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-NAME=$1
-PASSWORD=$2
-PORT=$3
-TAG=$4
+NAME="postgres"
+PASSWORD="uK8bzeWk"
+PORT="5432"
+TAG="12.1-alpine"
 
 if [ "$1" != "" ]; then
-    echo 'Usage: ./start_postgres.sh <containername> <dbpassword> <port> <tag>'
-    echo 'Example: ./start_postgres.sh postgres very_strong_postgres_password latest'
-    fi
-    
-sudo docker run --name $NAME --restart always \
+  NAME=$1
+fi
+if [ "$2" != "" ]; then
+  PASSWORD=$2
+fi
+if [ "$3" != "" ]; then
+  PORT=$3
+fi
+if [ "$4" != "" ]; then
+  TAG=$2
+fi
+
+docker run --name $NAME --restart always \
+  --network postgres-network \
   -e POSTGRES_PASSWORD=$PASSWORD \
   --mount src=$NAME,target=/var/lib/postgresql/data \
   -p $PORT:5432 \
   -d postgres:$TAG
-  
